a51c377d40f96e8dbe375ac4f65ea8cd1bb71a61
Dmitriy Kononov <kononov.dmitriy@gmail.com>
2020-06-02 12:53:01 +0600

    replacing Sirupsen by sirupsen

--- cmd/sftp/main.go
+++ cmd/sftp/main.go
@@ -11,7 +11,7 @@ import (
 
 	"golang.org/x/net/context"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/alecthomas/kingpin"
 	"github.com/chzyer/readline"
 	"github.com/google/shlex"
